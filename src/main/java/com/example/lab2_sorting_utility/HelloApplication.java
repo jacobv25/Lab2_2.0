acef8eead394c10e72d55ff41436f33f67f8dc9a
Adrian Bernardino <65195311+adrianb896@users.noreply.github.com>
2022-02-15 16:47:04 -0800

    Test 2
    trying to push to master

--- src/main/java/com/example/lab2_sorting_utility/HelloApplication.java
+++ src/main/java/com/example/lab2_sorting_utility/HelloApplication.java
@@ -16,6 +16,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
         // HELL JACOB
+        // Yooooooo
     }
 
     public static void main(String[] args) {
